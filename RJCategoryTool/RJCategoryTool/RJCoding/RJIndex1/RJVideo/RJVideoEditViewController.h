941b3f4042a7e3a4766a06b5cb45681e3b56f9f5
apple <353296573@qq.com>
2019-07-18 11:14:31 +0800

    补充

---
+++ RJCategoryTool/RJCategoryTool/RJCoding/RJIndex1/RJVideo/RJVideoEditViewController.h
@@ -0,0 +1,17 @@
+//
+//  RJVideoEditViewController.h
+//  RJCategoryTool
+//
+//  Created by apple on 2019/7/15.
+//  Copyright © 2019 Global Barter. All rights reserved.
+//
+
+#import "RJBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RJVideoEditViewController : RJBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
